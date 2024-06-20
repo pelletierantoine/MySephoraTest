862d8433dca3a7da1a29b4df73baf63848d031cd
Antoine Pelletier <pellet_ant@modulotech.fr>
2024-06-20 09:14:04 +0200

    fix: map

--- domain/src/main/java/com/pelletierantoine/mysephoratest/domain/usecases/ProductsWithReviewsAssociatedUseCase.kt
+++ domain/src/main/java/com/pelletierantoine/mysephoratest/domain/usecases/ProductsWithReviewsAssociatedUseCase.kt
@@ -13,34 +13,33 @@ class ProductsWithReviewsAssociatedUseCase(
 ) : NoParamsSuspendingUseCase<List<ProductWithReviews>>() {
 
     override suspend fun run(): Result<List<ProductWithReviews>> = runCatching {
-        val productWithReviews = mutableListOf<ProductWithReviews>()
         val products = productsUseCase.invoke().getOrThrow()
         val reviews = reviewsUseCase.invoke().getOrNull()
-        products.forEach { product ->
+
+        val productWithReviews: List<ProductWithReviews> = products.map { product ->
             val reviewProduct = reviews?.first { it.productId == product.productId }
             val rating = reviewProduct?.let {
                 val ratingReviews = it.reviews.filter { it.showStars }
                 ratingReviews.sumOf { it.rating.toDouble() } / ratingReviews.size
             }?.toFloat()
-            productWithReviews.add(
-                ProductWithReviews(
-                    productName = product.productName,
-                    numberRating = "${reviewProduct?.reviews?.size ?: 0} avis",
-                    brand = when (product.brand) {
-                        Brand.SEPHORA,
-                        Brand.CHANNEL -> product.brand.entireName
+            ProductWithReviews(
+                productName = product.productName,
+                numberRating = "${reviewProduct?.reviews?.size ?: 0} avis",
+                brand = when (product.brand) {
+                    Brand.SEPHORA,
+                    Brand.CHANNEL -> product.brand.entireName
 
-                        Brand.UNKNOWN -> ""
-                    },
-                    description = product.description,
-                    priceFormatted = product.price.toFormattedPrice(),
-                    imageUrl = product.imageUrls.small,
-                    reviews = reviewProduct?.reviews?.sortedByDescending { it.rating } ?: emptyList(),
-                    reviewsExpanded = false,
-                    rating = rating ?: 0f
-                )
+                    Brand.UNKNOWN -> ""
+                },
+                description = product.description,
+                priceFormatted = product.price.toFormattedPrice(),
+                imageUrl = product.imageUrls.small,
+                reviews = reviewProduct?.reviews?.sortedByDescending { it.rating } ?: emptyList(),
+                reviewsExpanded = false,
+                rating = rating ?: 0f
             )
         }
+
         return Result.success(productWithReviews)
     }
 }
